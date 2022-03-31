ee067c24749a6c1ef7bf2df60fcbc6456d17f822
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2022-03-31 07:19:00 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const number = 20;
+number > 20
+  ? console.log(`number is greater than ${20}`)
+  : number == 20
+  ? console.log(`number is equal to ${20}`)
+  : console.log(`number is bigger than ${20}`);
+console.log("this is my first node js code");
+console.log(__filename);
+console.log(require);
